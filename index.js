74abf38a5e6f629181c13841ff8b59b0e50cb270
Alan Burstein <alanburstein@Alans-MacBook-Pro.local>
2022-03-13 13:22:31 -0600

    move stuff

--- index.js
+++ index.js
@@ -3,11 +3,8 @@ const path = require("path");
 
 const app = express();
 const port = process.env.PORT || "8000";
-app.use(express.static(__dirname));
+app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname+"/index.html"), { title: "Home" });
-});
 
 app.listen(port, () => {
   console.log(`Listening to requests on http://localhost:${port}`);
